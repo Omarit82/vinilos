358c6be3c8e34f8899975938331d909d4799b516
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2024-09-24 20:18:07 -0300

    EntregaFinal+Roselli

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,8 +12,8 @@ export const Header = ({titulo}) => {
                 <img className="logo m-2" src={ LogoImg } alt="Logo de Vinilos" />
             </Link>
             <div className='d-flex flex-column flex-md-row justify-content-around align-items-center w-100'>
-                <h1 className="text-center ms-3 me-3">{ titulo }</h1>
-                <form action="" className='w-md-50 w-75 d-flex m-2'>
+                <h1 className="text-center ms-3 me-3 fs-md-4 fs-5">{ titulo }</h1>
+                <form action="" className='w-md-50 w-75 d-flex m-1 m-md-2'>
                     <input type="text" name="busqueda" placeholder=' ...busqueda' className='w-md-75 w-100 me-3' />
                     <button type="submit" className='botonesHeader'><img src={ SearchImg } alt="Busqueda" className="headerImg" /></button>
                 </form>
