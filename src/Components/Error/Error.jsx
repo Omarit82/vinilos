c6baed51266ed1dd596ca641431bb18cf0efc46c
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2024-09-24 15:02:57 -0300

    EntregaFinal+Roselli

--- src/Components/Error/Error.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export const Error = () => {
-    return(
-        <main>
-            <h1>Ha ocurrido un error</h1>
-        </main>
-    )
-}
--- src/Components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-export const Footer = () =>{
-    return(
-        <div className="footer">
-            <h3>Powered by me</h3>
-        </div>
-    )
-}
