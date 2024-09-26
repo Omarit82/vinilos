8839ba981f6d97f0e35f8fa5f6cd46a5b8ce563e
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2024-09-26 14:38:03 -0300

    EntregaFinal+Roselli

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -7,10 +7,11 @@ export const Item = ( {item} ) => {
             <div className='d-flex flex-column justify-content-center align-items-center'>
                 <h3 className='text-center mt-3'>{ item.titulo }</h3> 
                 <h4>{ item.autor }</h4>
-                <h4 className="fs-5">{ item.anioRelease }</h4>
-                <div className="d-flex"><p>{item.Formato[0]}</p>
-                {item.Formato[1]&&<p>-{item.Formato[1]}</p>}
+                <h4 className="fs-5 mb-1">{ item.anioRelease }</h4>
+                <div className="d-flex"><p className="mb-0">{item.Formato[0]}</p>
+                {item.Formato[1]&&<p className="mb-0">-{item.Formato[1]}</p>}
                 </div>
+                <p className="text-center m-0">{item.genero}</p>
             </div>
             <img src={item.imagen} alt={item.titulo} className='imgDisco m-1' />
             <Link to={`/detail/${item.id}`} className='btn btn-info mb-4'>Mas Info</Link>
